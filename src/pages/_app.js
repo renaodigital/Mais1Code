bb44629397eaa6c981c954cf104199296235347b
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 03:24:45 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ export default function MyApp({ Component, pageProps }) {
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-99JF8KBFVZ');
-    `
+                `
             }
 
         </Script>
